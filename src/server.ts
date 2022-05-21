66cbc81243971f4eec237776238509e420e812c1
Danilo <danilomartins.pacs@gmail.com>
2022-05-21 13:22:18 -0300

    chore: install dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,8 @@
+import 'dotenv/config';
 import * as http from 'http';
 import app from './app';
-const PORT = process.env.PORT || "3339";
+
+const PORT = process.env.PORT || "3338";
 
 http.createServer(app).listen(PORT,()=>{
   console.log('Express server listen: '+PORT);
